87048cc0e376f9de8d883c0d437e18fad4991070
Ayax Córdova <ayax.cordova@aydev.mx>
2023-11-02 13:49:47 -0600

    add solved problem
    This commit also removes the files for PHP and JS, because this
    challenge is intended for Python only

--- code/collections/counter/README.md
+++ code/collections/counter/README.md
@@ -1,5 +1,7 @@
 # [collection.Counter](https://docs.python.org/2/library/collections.html#collections.Counter)
 
+> This challenge is for Python only
+
 A counter is a container that stores elements as dictionary keys, and their counts are stored as dictionary values.
 
 #### Sample Code
@@ -27,7 +29,7 @@ $Raghu$ is a shoe shop owner. His shop has $X$ number of shoes.
 
 He has a list containing the size of each shoe he has in his shop.
 
-There are $N$ number of customers who are willing to pay $x_1$ amount of money only if they get the shoe of their desired size.
+There are $N$ number of customers who are willing to pay $x_i$ amount of money only if they get the shoe of their desired size.
 
 Your task is to compute how much money $Raghu$ earned.
 
--- code/collections/counter/main.py
+++ code/collections/counter/main.py
@@ -1,4 +1,37 @@
 import os
+from collections import Counter
+
+def counter(shoe_sizes: list, customers: list):
+    earnings = 0
+
+    for customer in customers:
+        size, price = customer.split(' ')
+        size, price = int(size), int(price)
+
+        if shoe_sizes.get(size) is None or shoe_sizes.get(size) < 1:
+            continue
+
+        shoe_sizes.subtract([size])
+        earnings += price
+
+    return earnings
+
 
 if __name__ == '__main__':
-    pass
+    root_dir = os.path.join(os.path.dirname(__file__), 'input')
+
+    for root, _, files in os.walk(root_dir):    
+        for file in files:
+            with open(os.path.join(root, file), 'r') as f:
+                input_values = f.readlines()
+            
+            shoe_sizes = Counter([int(x) for x in input_values[1].split(' ')])
+            customers  = input_values[3:-1]
+            result     = int(input_values[-1])
+
+            ans = counter(shoe_sizes, customers)
+
+            if ans != result:
+                print(f'The answer is incorrect: answer({ans}) != result({result})')
+            else:
+                print(f'{file}: Raghu earn -> {ans}')
